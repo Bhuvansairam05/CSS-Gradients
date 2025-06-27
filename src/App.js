f3a85f8afa960cc94638c0e216e73789a3f3c010
Bhuvansairam05 <bhuvanasairam2005@gmail.com>
2025-06-27 22:25:20 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <> 
+    <br></br>
+    <h1 className='Blue'>Hello world <span>hover to see effect</span></h1>
+    <br></br><br></br>
+    <h2>Below is the Example CSS Linear Gradient</h2><br></br><br></br>
+    <div class="Gradient">Gradient</div>
+    </>
+  );
+}
+
+export default App;
